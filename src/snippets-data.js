7bb802b2f0f4b9246a4f614b301326efc4483dd2
nice <piruz90@gmail.com>
2025-10-17 19:57:02 -0700

    Update: 2025-10-17 19:57 - Auto-generated snippets

--- src/snippets-data.js
+++ src/snippets-data.js
@@ -1,19 +1,11 @@
 // snippets-data.js
 // Auto-generated from image folders (sorted by creation date)
-// Last updated: 2025-10-18T02:53:14.327Z
+// Last updated: 2025-10-18T02:57:02.275Z
 
 export const SNIPPET_DATA = [
   {
-    "id": "Artemisia",
-    "title": "1. Artemisia",
-    "imageFilenames": [
-      "Artemisia-californica-Native.jpg"
-    ],
-    "defaultNote": ""
-  },
-  {
     "id": "Daisy-looking-short-Neighbor-01",
-    "title": "2. Daisy Looking Short Neighbor",
+    "title": "1. Daisy Looking Short Neighbor",
     "imageFilenames": [
       "20251011_81060211.jpg"
     ],
@@ -21,7 +13,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Flower-cv-plaza-01",
-    "title": "3. Flower Cv Plaza",
+    "title": "2. Flower Cv Plaza",
     "imageFilenames": [
       "20251011_131717.jpg",
       "20251011_131736.jpg"
@@ -30,7 +22,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Flower-cv-plaza-02",
-    "title": "4. Flower Cv Plaza",
+    "title": "3. Flower Cv Plaza",
     "imageFilenames": [
       "20251011_131746.jpg",
       "20251011_131750.jpg"
@@ -39,7 +31,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Flower-short-cv-plaza",
-    "title": "5. Flower Short Cv Plaza",
+    "title": "4. Flower Short Cv Plaza",
     "imageFilenames": [
       "20251011_131802.jpg"
     ],
@@ -47,7 +39,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Ground-cover-Neighbor-01",
-    "title": "6. Ground Cover Neighbor",
+    "title": "5. Ground Cover Neighbor",
     "imageFilenames": [
       "20251011_110409.jpg",
       "20251011_110415.jpg"
@@ -56,7 +48,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Lantanas-Neighbor-01",
-    "title": "7. Lantanas Neighbor",
+    "title": "6. Lantanas Neighbor",
     "imageFilenames": [
       "20251017_182251.jpg",
       "20251017_182255.jpg",
@@ -66,7 +58,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Shrub-cv-plaza-ground-cover-02",
-    "title": "8. Shrub Cv Plaza Ground Cover",
+    "title": "7. Shrub Cv Plaza Ground Cover",
     "imageFilenames": [
       "20251011_1026512.jpg"
     ],
@@ -74,7 +66,7 @@ export const SNIPPET_DATA = [
   },
   {
     "id": "Shrub-white-flower-Neighbor-01",
-    "title": "9. Shrub White Flower Neighbor",
+    "title": "8. Shrub White Flower Neighbor",
     "imageFilenames": [
       "20251011_110314.jpg",
       "20251011_110320.jpg"
